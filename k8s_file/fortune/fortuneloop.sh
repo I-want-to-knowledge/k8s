bf55328e50ac651273ccccedcacc861e5f252791
YanZhen <yz01180003@alibaba-inc.com>
2021-11-19 18:06:49 +0800

    学习configMap

--- k8s_file/fortune/fortuneloop.sh
+++ k8s_file/fortune/fortuneloop.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 trap "exit" SIGINT
+v=$1
+[ ! "$v" ] && v=20
+echo $v seconds!!!
 mkdir -p /var/htdocs
 
 while :
 do
+	# shellcheck disable=SC2046
 	echo $(date) Writing fortune to /var/htdocs/index.html
 	/usr/games/fortune > /var/htdocs/index.html
-	sleep 10
+	sleep $v
 done
