c568300fc519aae9e1f2098143925013d6758525
YanZhen <yz01180003@alibaba-inc.com>
2021-12-03 15:33:47 +0800

    k8s，Fabric8操作v5.*！

--- k8s_file/java/operate/Fabric8.java
+++ k8s_file/java/operate/Fabric8.java
@@ -1,6 +1,8 @@
 package operate;
 
+import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodBuilder;
 import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
@@ -15,33 +17,40 @@ import java.util.Arrays;
  */
 public class Fabric8 {
     public static void main(String[] args) throws InterruptedException {
-        // 版本号 io.fabric8.kubernetes-client:4.13.3
-//        KubernetesClient client = new DefaultKubernetesClient();
-//
-//        // list pods in the default namespace
-//        PodList pods = client.pods().inNamespace("default").list();
-//        pods.getItems().forEach(s -> System.out.println("Found pod：" + s.getMetadata().getName()));
-//
-//        // create a pod
-//        System.out.println("Create a pod");
-//        Pod pod = client.pods().inNamespace("default").createNew()
-//                .withNewMetadata().withName("programmatically-created-pod").endMetadata()
-//                .withNewSpec()
-//                .addNewContainer().withName("main").withImage("busybox").withCommand(Arrays.asList("sleep", "99999")).endContainer()
-//                .endSpec()
-//                .done();
-//        System.out.println("Create pod : " + pod);
-//
-//        // edit the pod (add a label to it)
-//        client.pods().inNamespace("default").withName("programmatically-created-pod")
-//                .edit().editMetadata().addToLabels("foo", "bar").endMetadata().done();
-//        System.out.println("Added label foo=bar to pod");
-//
-//        System.out.println("Waiting 1 minute before deleting pod...");
-//        Thread.sleep(60000);
-//
-//        // delete the pod
-//        client.pods().inNamespace("default").withName("programmatically-created-pod").delete();
-//        System.out.println("Deleted the pod");
+        // 版本号 io.fabric8.kubernetes-client
+        KubernetesClient client = new DefaultKubernetesClient();
+        String namespace = "default";
+        String podName = "programmatically-created-pod";
+
+
+        // list pods in the default namespace
+        PodList pods = client.pods().inNamespace(namespace).list();
+        pods.getItems().forEach(s -> System.out.println("Found pod：" + s.getMetadata().getName()));
+
+        // create a pod
+        System.out.println("Create a pod");
+        Pod pod = new PodBuilder()
+                .withNewMetadata().withNamespace(namespace).withName(podName).endMetadata()
+                .withNewSpec()
+                .addNewContainer()
+                .withName("main").withImage("busybox").withCommand(Arrays.asList("sleep", "99999"))
+                .endContainer()
+                .endSpec()
+                .build();
+        ObjectMeta metadata = pod.getMetadata();
+        client.pods().inNamespace(metadata.getNamespace()).createOrReplace(pod);
+        System.out.println("Create pod : " + pod);
+
+        // edit the pod (add a label to it)
+        client.pods().inNamespace(metadata.getNamespace()).withName(metadata.getName())
+                .edit(pod1 -> new PodBuilder(pod1).editMetadata().addToLabels("foo", "bar").endMetadata().build());
+        System.out.println("Added label foo=bar to pod");
+
+        System.out.println("Waiting 1 minute before deleting pod...");
+        Thread.sleep(60000);
+
+        // delete the pod
+        client.pods().inNamespace(metadata.getNamespace()).withName(metadata.getName()).delete();
+        System.out.println("Deleted the pod");
     }
 }
